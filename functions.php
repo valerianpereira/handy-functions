5bd3963bebb08d7c0992827337308f319b47c75d
Valerian Pereira <valerianpereira25@gmail.com>
2018-10-24 22:05:15 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -36,3 +36,11 @@ function jsonValidator($data = NULL) {
   }
   return false;
 }
+
+function escapeDBInfo($value) {
+
+$search = array("\\",  "\x00", "\n",  "\r",  "'",  '"', "\x1a");
+$replace = array("\\\\","\\0","\\n", "\\r", "\'", '\"', "\\Z");
+
+return str_replace($search, $replace, $value);
+}
